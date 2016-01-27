36d389f1d43a4d494bf958caa4e96229d6fa1f8f
Claudio Santoro <claudio.santoro@redes.unb.br>
2016-01-27 12:20:31 -0200

    Fixed Proxy

--- Yupi/Core/Security/CrossDomainSettings.cs
+++ Yupi/Core/Security/CrossDomainSettings.cs
@@ -39,7 +39,7 @@ namespace Yupi.Core.Security
                 Yupi.GetDefaultEncoding()
                     .GetBytes(
                         "<?xml version=\"1.0\"?>\r\n<!DOCTYPE cross-domain-policy SYSTEM \"/xml/dtds/cross-domain-policy.dtd\">\r\n<cross-domain-policy>\r\n<allow-access-from domain=\"*\" to-ports=\"" +
-                        ServerConfigurationSettings.Data["game.tcp.port"] + "\" />\r\n</cross-domain-policy>\0");
+                        ServerConfigurationSettings.Data["game.tcp.port.proxy"] + "\" />\r\n</cross-domain-policy>\0");
         }
     }
 }
