40fae1078700779751085943a15be4d9289da5c7
TheDoct0r11 <thedoct0r@protonmail.com>
2016-09-14 18:55:09 +0000

    Header

---
+++ Tools/copyright.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# updates the copyright information for all .cs files
+# usage: call recursive_traversal, with the following parameters
+# parent directory, old copyright text content, new copyright text content
+
+import os
+
+excludedir = ["..\\Lib"]
+
+def update_source(filename, oldcopyright, copyright):
+    utfstr = chr(0xef)+chr(0xbb)+chr(0xbf)
+    fdata = file(filename,"r+").read()
+
+    copyright = copyright.replace("${File}", os.path.basename(filename))
+    oldcopyright = oldcopyright.replace("${File}", os.path.basename(filename))
+
+    isUTF = False
+    if (fdata.startswith(utfstr)):
+        isUTF = True
+        fdata = fdata[3:]
+    if (oldcopyright != None):
+        if (fdata.startswith(oldcopyright)):
+            fdata = fdata[len(oldcopyright):]
+    if not (fdata.startswith(copyright)):
+        print "updating "+filename
+        fdata = copyright + fdata
+        if (isUTF):
+            file(filename,"w").write(utfstr+fdata)
+        else:
+            file(filename,"w").write(fdata)
+
+def recursive_traversal(dir,  oldcopyright, copyright):
+    global excludedir
+    fns = os.listdir(dir)
+    print "listing "+dir
+    for fn in fns:
+        fullfn = os.path.join(dir,fn)
+        if (fullfn in excludedir):
+            continue
+        if (os.path.isdir(fullfn)):
+            recursive_traversal(fullfn, oldcopyright, copyright)
+        else:
+            if (fullfn.endswith(".cs")):
+                update_source(fullfn, oldcopyright, copyright)
+
+
+dir = os.path.dirname(os.path.abspath(__file__))
+
+
+oldcright = file(dir + "/oldcr.txt","r+").read()
+cright = file(dir + "/copyrightText.txt","r+").read()
+
+dir = os.path.dirname(dir)
+
+files = os.listdir(dir)
+
+for fn in files:
+    fullname = os.path.join(dir, fn)
+
+    if(fn.startswith("Yupi.") and os.path.isdir(fullname)):
+        print "#######" + fn + "#######"
+        recursive_traversal(fullname, oldcright, cright)
+exit()
--- Yupi.Model/Domain/Navigator/NavigatorCategory.cs
+++ Yupi.Model/Domain/Navigator/NavigatorCategory.cs
@@ -1,31 +1,27 @@
-﻿#region Header
-
-/**
-     Because i love chocolat...
-                                    88 88
-                                    "" 88
-                                       88
-8b       d8 88       88 8b,dPPYba,  88 88
-`8b     d8' 88       88 88P'    "8a 88 88
- `8b   d8'  88       88 88       d8 88 ""
-  `8b,d8'   "8a,   ,a88 88b,   ,a8" 88 aa
-    Y88'     `"YbbdP'Y8 88`YbbdP"'  88 88
-    d8'                 88
-   d8'                  88
-
-   Private Habbo Hotel Emulating System
-   @author Claudio A. Santoro W.
-   @author Kessiler R.
-   @version dev-beta
-   @license MIT
-   @copyright Sulake Corporation Oy
-   @observation All Rights of Habbo, Habbo Hotel, and all Habbo contents and it's names, is copyright from Sulake
-   Corporation Oy. Yupi! has nothing linked with Sulake.
-   This Emulator is Only for DEVELOPMENT uses. If you're selling this you're violating Sulakes Copyright.
-*/
-
-#endregion Header
-
+﻿// ---------------------------------------------------------------------------------
+// <copyright file="NavigatorCategory.cs" company="https://github.com/sant0ro/Yupi">
+//   Copyright (c) 2016 Claudio Santoro, TheDoctor
+// </copyright>
+// <license>
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
+//
+//   The above copyright notice and this permission notice shall be included in
+//   all copies or substantial portions of the Software.
+//
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//   THE SOFTWARE.
+// </license>
+// ---------------------------------------------------------------------------------
 namespace Yupi.Model.Domain
 {
     using System.Collections.Generic;
